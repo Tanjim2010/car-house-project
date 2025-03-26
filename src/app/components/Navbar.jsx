97fe18a5b74ee767aec43c5b06435a3d443a24c2
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-03-26 12:00:44 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li>home</li>
+            <li>any</li>
+        </ul>
+    );
+};
+
+export default Navbar;
